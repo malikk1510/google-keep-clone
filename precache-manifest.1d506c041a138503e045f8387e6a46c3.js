9d8ac935de5a7f101755e007880f95bce1b94b13
malikk1510 <69068011+malikk1510@users.noreply.github.com>
2020-09-29 17:11:38 +0530

    Updates

---
+++ precache-manifest.1d506c041a138503e045f8387e6a46c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef495d8507349161e0c6dfe8faa6396b",
+    "url": "/google-keep-clone/index.html"
+  },
+  {
+    "revision": "69a5941935890a085fc9",
+    "url": "/google-keep-clone/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "0a9f62a8e4f073f3a4cf",
+    "url": "/google-keep-clone/static/css/main.678ae944.chunk.css"
+  },
+  {
+    "revision": "69a5941935890a085fc9",
+    "url": "/google-keep-clone/static/js/2.1eb95538.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/google-keep-clone/static/js/2.1eb95538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a9f62a8e4f073f3a4cf",
+    "url": "/google-keep-clone/static/js/main.7166f446.chunk.js"
+  },
+  {
+    "revision": "1dcca35a7a6377bd82c0",
+    "url": "/google-keep-clone/static/js/runtime-main.146f3496.js"
+  }
+]);
